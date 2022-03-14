8bff998a5756e3ee1d0481b9d6ce6ea0e86d3a02
Dejlis <dejlislives@gmail.com>
2022-03-14 18:16:16 +0100

    some

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 module.exports = app;
