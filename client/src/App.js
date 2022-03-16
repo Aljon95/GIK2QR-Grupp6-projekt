8b849cca5a0ac80eed1d409c63cab4c876fe695c
Dejlis <dejlislives@gmail.com>
2022-03-16 11:10:52 +0100

    app change

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,37 @@ import itemDetail from './Views/itemDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <h1>auktion</h1>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              <Link to='/'>Hem</Link>
+            </Typography>
+            <Button color='inherit'>
+              <Link to='/items/'>Visa alla saker</Link>
+            </Button>
+            <Button color='inherit'>
+              <Link to='/items/new'>Skapa sak</Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Box
+          sx={{
+            maxWidth: '60rem',
+            margin: '1rem auto'
+          }}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/items/new' component={itemEdit} />
+            <Route exact path='/items/:id' component={itemDetail} />
+            <Route exact path='/items/:id/edit' component={itemEdit} />
+            <Route exact path='/items/' component={items} />
+          </Switch>
+        </Box>
+      </Router>
     </div>
   );
 }
