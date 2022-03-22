ec6e2aaf1ac2688e72019b90bf42f395f40042d0
Aljon95 <74709420+Aljon95@users.noreply.github.com>
2022-03-22 13:13:55 +0100

    i am mvp

---
+++ client/src/Components/ItemCreate.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState} from 'react';
+import ResourceModel from '../models/resourceModel';
+import { TextField, Button, Chip, Grid } from '@mui/material';
+
+
+
+
+
+
+
+export default function ItemCreate({onCreate}) {
+    const [userId, setUserId] = useState('');
+    const [title, setTitle] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const [description, setDescription] = useState('');
+    const [startingPrice, setStartingPrice] = useState('');
+    return(
+        <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+          style={{backgroundColor: "#e6e6e6"}}
+            name='userId'
+            label='användar id'
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+          style={{backgroundColor: "#e6e6e6"}}
+            name='title'
+            label='Titel'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+          style={{backgroundColor: "#e6e6e6"}}
+            name='imageUrl'
+            label='Bildlänk (max 200 tecken)'
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            fullWidth
+            multiline
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+          style={{backgroundColor: "#e6e6e6"}}
+            name='description'
+            label='Innehåll'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            fullWidth
+            multiline
+            minRows={7}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+          style={{backgroundColor: "#e6e6e6"}}
+            name='startingPrice'
+            label='utgångspris'
+            value={startingPrice}
+            onChange={(e) => setStartingPrice(e.target.value)}
+            fullWidth
+          />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{ display: 'flex', justifyContent: 'flex-end', gap: '.4rem' }}>
+          <Button variant='contained' color='primary' onClick={() => {
+          onCreate({title,imageUrl,description,startingPrice,userId});
+          console.log(userId, title,imageUrl,description,startingPrice)}}>
+            Lägg upp
+          </Button>
+        </Grid>
+      </Grid>
+    )}
--- client/src/Views/itemCreate.js
+++ client/src/Views/itemCreate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ResourceModel from '../models/resourceModel';
 import { TextField, Button, Chip, Grid } from '@mui/material';
+import ItemCreate from '../Components/ItemCreate';
 export default class itemCreate extends React.Component {
   state = { item: {userId: '', title: '', description: '', startingPrice: '',imageUrl: '', seller: {}, } };
   resourceModel = null;
@@ -17,79 +18,17 @@ export default class itemCreate extends React.Component {
     const value = e.target.value;
     this.setState({ item: { ...this.state.item, [field]: value } });
   }
-  onSave() {
+  onSave(newItem) {
       const itemWithUserId = { ...this.state.item, userId: this.state.item.userId };
-      this.resourceModel.create(itemWithUserId).then((result) => {
+      console.log(newItem)
+      this.resourceModel.create({newItem}).then((result) => {
         console.log(result);
         window.location.href = `/items/${result.id}`;
       });
   }
   render() {
-    const item = this.state.item;
     return (
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-          style={{backgroundColor: "#e6e6e6"}}
-            name='userId'
-            label='användar id'
-            value={item.userId}
-            onChange={this.onChange}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-          style={{backgroundColor: "#e6e6e6"}}
-            name='title'
-            label='Titel'
-            value={item.title}
-            onChange={this.onChange}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-          style={{backgroundColor: "#e6e6e6"}}
-            name='imageUrl'
-            label='Bildlänk (max 200 tecken)'
-            value={item.imageUrl}
-            onChange={this.onChange}
-            fullWidth
-            multiline
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-          style={{backgroundColor: "#e6e6e6"}}
-            name='description'
-            label='Innehåll'
-            value={item.description}
-            onChange={this.onChange}
-            fullWidth
-            multiline
-            minRows={7}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-          style={{backgroundColor: "#e6e6e6"}}
-            name='startingPrice'
-            label='utgångspris'
-            value={item.startingPrice}
-            onChange={this.onChange}
-            fullWidth
-          />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sx={{ display: 'flex', justifyContent: 'flex-end', gap: '.4rem' }}>
-          <Button variant='contained' color='primary' onClick={this.onSave}>
-            Lägg upp
-          </Button>
-        </Grid>
-      </Grid>
+<ItemCreate onCreate={this.onSave} /> 
     );
   }
 }
