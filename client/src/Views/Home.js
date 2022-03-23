978faf565081789e31f15121bd509f0d7e8e1cfb
Aljon95 <74709420+Aljon95@users.noreply.github.com>
2022-03-23 12:52:46 +0100

    home

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -1,6 +1,7 @@
+import { border, borderRadius } from '@mui/system'
 import React from 'react'
 
 export default function Home() {
-  return (<div>Home</div>
+  return (<div style={{border: "solid " , borderRadius: "20px", boxShadow: "2px 10px 2px 4px"}}>Välkommna till världens vackraste auktionsida!</div>
   )
 }
